a5f81f769ffe93519a63e81f83cc5e93b8c414f2
shilpa <shlp.12129@gmail.com>
2025-08-11 08:54:01 -0700

    LC

--- 6.merge2SortedLists.py
+++ 6.merge2SortedLists.py
@@ -1,5 +1,6 @@
 # Easy 
 # Merge Two Sorted Lists
+# Singly linked list - Two-pointer merge
 
 # You are given the heads of two sorted linked lists list1 and list2.
 # Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
