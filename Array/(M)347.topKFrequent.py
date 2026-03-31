14784a5c00d70248516caac461c89fb844ad4594
wadeshilpa <160187057+wadeshilpa@users.noreply.github.com>
2026-03-30 18:22:33 -0700

    Add problem description and examples to topKFrequent.py
    Added problem description and input/output examples for clarity.

--- Array/(M)347.topKFrequent.py
+++ Array/(M)347.topKFrequent.py
@@ -2,6 +2,13 @@
 # Top K Frequent Elements
 # https://leetcode.com/problems/top-k-frequent-elements/description/
 
+# Given an integer array nums and an integer k,
+# return the k most frequent elements.
+# You may return the answer in any order.
+
+# Input: nums = [1,1,1,2,2,3], k = 2         Output: [1, 2]
+########################################################################
+
 class Solution:
     def topKFrequent(self, nums:list[int],k:int)->list[int]:
         frequency = {}
